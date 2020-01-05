68c6eeaed7e60b367f87cc077171d2d7f62ff9fb
Harry Chen <czy88840616@gmail.com>
2020-01-05 21:38:03 +0800

    fix: add pkg

--- packages/serverless-invoke/test/index.test.ts
+++ packages/serverless-invoke/test/index.test.ts
@@ -3,7 +3,6 @@ import { join } from 'path';
 import * as assert from 'assert';
 describe('/test/index.test.ts', () => {
   it('should use origin http trigger', async () => {
-    // TODO 这个有问题，要重构，暂时不改了
     const result: any = await invoke({
       provider: 'fc',
       trigger: 'http',
@@ -11,6 +10,6 @@ describe('/test/index.test.ts', () => {
       functionName: 'http',
       data: [{ name: 'params' }],
     });
-    assert(result === 'hello http world');
+    assert(result && result.body === 'hello http world');
   });
 });
