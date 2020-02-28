070c92adc8a9268fcc0d8afcf4d38c5878192ac6
echosoar <82163514@qq.com>
2020-02-28 16:55:02 +0800

    feat meat json

---
+++ packages/serverless-meta-json/test/index.test.ts
@@ -0,0 +1,36 @@
+import { simpleGenerator } from '../src';
+import { resolve } from 'path';
+import * as assert from 'assert';
+
+describe('/test/index.test.ts', () => {
+  it('simpleGenerator', async () => {
+    const meta = await simpleGenerator(
+      resolve(__dirname, 'archives'),
+      {
+        service: {
+          name: 'test',
+          fnPrefix: 'prefix'
+        },
+        functions: {
+          a: {
+            handler: 'a.handler',
+            events: [ { http: { path: '/api/a', method: [] }}]
+          },
+          b: {
+            handler: 'b.handler',
+            events: [ { http: { path: '/api/b', method: ['GET'] }}]
+          }
+        },
+        custom: {
+          customDomain: {
+            domainName: 'meta-test.example.com'
+          }
+        }
+      }
+    );
+    assert(meta.gateway.paths['/api/a'].ALL['x-gateway-intergration'].url.group === 'test');
+    assert(meta.gateway.paths['/api/a'].ALL['x-gateway-intergration'].url.name === 'prefix-a');
+    assert(meta.gateway.paths['/api/b'].GET['x-gateway-intergration'].url.name === 'prefix-b');
+    assert(meta.gateway['x-gateway-domain'] === 'meta-test.example.com');
+  });
+});
