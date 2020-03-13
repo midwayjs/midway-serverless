28641ba23926db28366cf4fb5be5d6c9c3080a7c
echosoar <82163514@qq.com>
2020-03-13 16:20:55 +0800

    fix: invoke copy file

--- packages/faas-util-ts-compile/src/utils.ts
+++ packages/faas-util-ts-compile/src/utils.ts
@@ -1,4 +1,11 @@
-const getType = (v: any) => (({}).toString.call(v).slice(8, -1).toLowerCase());
+import * as globby from 'globby';
+import { existsSync, copy, statSync } from 'fs-extra';
+import { join } from 'path';
+const getType = (v: any) =>
+  ({}.toString
+    .call(v)
+    .slice(8, -1)
+    .toLowerCase());
 export const combineTsConfig = (config, ...tsConfig: any[]) => {
   const combined = config || {};
   for (const config of tsConfig || []) {
@@ -20,4 +27,88 @@ export const combineTsConfig = (config, ...tsConfig: any[]) => {
     }
   }
   return combined;
+};
+
+// 符合sourceGlob条件的文件中 是否存在 比所有符合toGlob条件的文件 要新的文件
+// 返回 fromGlob 中更新的文件
+export const compareFileChange = async (
+  fromGlob: string[],
+  toGlob: string[],
+  options?: any
+) => {
+  options = options || {};
+  if (!options.cwd) {
+    options.cwd = process.cwd();
+  }
+  options.stats = true;
+  const fromFiles: any = await globby(fromGlob, options);
+  const toFiles: any = await globby(toGlob, options);
+
+  if (!fromFiles || !fromFiles.length) {
+    return [];
+  }
+
+  if (!toFiles || !toFiles.length) {
+    return fromFiles.map((file: any) => file.path);
+  }
+  let latestFilesChangeTime = 0;
+  for (const file of toFiles) {
+    if (file.stats.mtimeMs > latestFilesChangeTime) {
+      latestFilesChangeTime = file.stats.mtimeMs;
+    }
+  }
+  const result = [];
+  for (const file of fromFiles) {
+    if (file.stats.mtimeMs > latestFilesChangeTime) {
+      result.push(file.path);
+    }
+  }
+  return result;
+};
+
+interface ICopyOptions {
+  sourceDir: string;
+  targetDir: string;
+  defaultInclude?: string[];
+  include?: string[];
+  exclude?: string[];
+  log?: (path: string) => void;
+}
+
+export const copyFiles = async (options: ICopyOptions) => {
+  const {
+    defaultInclude,
+    include,
+    exclude,
+    sourceDir,
+    targetDir,
+    log,
+  } = options;
+  const paths = await globby(
+    (defaultInclude || ['*.yml', '*.js', '*.json', 'app', 'config']).concat(
+      include || []
+    ),
+    {
+      cwd: sourceDir,
+      ignore: ['node_modules/**/**'].concat(exclude || []),
+    }
+  );
+  await Promise.all(
+    paths.map((path: string) => {
+      const source = join(sourceDir, path);
+      const target = join(targetDir, path);
+      if (existsSync(target)) {
+        const sourceStat = statSync(source);
+        const targetStat = statSync(target);
+        // source 修改时间小于目标文件 修改时间，则不拷贝
+        if (sourceStat.mtimeMs <= targetStat.mtimeMs) {
+          return;
+        }
+      }
+      if (log) {
+        log(path);
+      }
+      return copy(source, target);
+    })
+  );
 };
