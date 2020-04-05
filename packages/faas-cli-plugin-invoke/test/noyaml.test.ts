affd458ab619b227517729710a06ebfdb69d0349
echosoar <82163514@qq.com>
2020-04-05 13:46:38 +0800

    fix: #107

--- packages/faas-cli-plugin-invoke/test/noyaml.test.ts
+++ packages/faas-cli-plugin-invoke/test/noyaml.test.ts
@@ -4,38 +4,34 @@ import * as assert from 'assert';
 import { remove } from 'fs-extra';
 describe('/test/noyaml.test.ts', () => {
   it('getFuncList', async () => {
-    const baseDir = join(__dirname, 'fixtures/noYaml');
-    await remove(join(baseDir, './.faas_debug_tmp'));
-    const result: any = await (getFuncList as any)({
-      functionDir: join(__dirname, 'fixtures/noYaml'),
-    });
+    const functionDir = join(__dirname, 'fixtures/noYaml');
+    await remove(join(functionDir, './.faas_debug_tmp'));
+    const result: any = await getFuncList({ functionDir });
     assert(result.service && result.service.handler === 'service.handler' && result['service2-index'].events[0].http.path === '/api/test2');
   });
   it('TrippleGetFuncList', async () => {
-    const baseDir = join(__dirname, 'fixtures/noYaml');
-    await remove(join(baseDir, './.faas_debug_tmp'));
+    const functionDir = join(__dirname, 'fixtures/noYaml');
+    await remove(join(functionDir, './.faas_debug_tmp'));
     const result = await Promise.all([0, 1, 2].map(() => {
-      return getFuncList({
-        functionDir: join(__dirname, 'fixtures/noYaml')
-      });
+      return getFuncList({ functionDir });
     }));
     assert(result.length === 3 && result[2].service);
   });
   it('invoke', async () => {
-    const baseDir = join(__dirname, 'fixtures/noYaml');
-    await remove(join(baseDir, './.faas_debug_tmp'));
-    const result: any = await (invoke as any)({
-      functionDir: join(__dirname, 'fixtures/noYaml'),
+    const functionDir = join(__dirname, 'fixtures/noYaml');
+    await remove(join(functionDir, './.faas_debug_tmp'));
+    const result: any = await invoke({
+      functionDir,
       functionName: 'service'
     });
     assert(result.body === 'hello world');
   });
   it('doubleInvoke', async () => {
-    const baseDir = join(__dirname, 'fixtures/noYaml');
-    await remove(join(baseDir, './.faas_debug_tmp'));
+    const functionDir = join(__dirname, 'fixtures/noYaml');
+    await remove(join(functionDir, './.faas_debug_tmp'));
     const result = await Promise.all([0, 1].map(() => {
       return invoke({
-        functionDir: join(__dirname, 'fixtures/noYaml'),
+        functionDir,
         functionName: 'service'
       });
     }));
