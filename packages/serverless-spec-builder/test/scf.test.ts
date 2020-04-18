887c6c016a754b147baa88b700d47f1104546d3a
Harry Chen <czy88840616@gmail.com>
2020-04-19 00:30:37 +0800

    test: add scf trigger case

--- packages/serverless-spec-builder/test/scf.test.ts
+++ packages/serverless-spec-builder/test/scf.test.ts
@@ -84,5 +84,105 @@ describe('/test/scf.test.ts', () => {
         },
       });
     });
+
+    it('test transform timer event', () => {
+      const result = generateFunctionsSpec(
+        path.join(__dirname, './fixtures/scf/f-event-timer.yml')
+      );
+      assert.deepStrictEqual(result, {
+        service: 'serverless-hello-world',
+        provider: {
+          name: 'tencent',
+          runtime: 'Node.js10.15',
+          memorySize: 128,
+          timeout: 3,
+        },
+        functions: {
+          index: {
+            handler: 'index.handler',
+            runtime: 'Node.js10.15',
+            timeout: 3,
+            memorySize: 128,
+            events: [
+              {
+                timer: {
+                  name: 'timer',
+                  parameters: { cronExpression: '*/5 * * * *', enable: true },
+                },
+              },
+            ],
+          },
+        },
+      });
+    });
+
+    it('test transform cos event', () => {
+      const result = generateFunctionsSpec(
+        path.join(__dirname, './fixtures/scf/f-event-cos.yml')
+      );
+
+      assert.deepStrictEqual(result, {
+        service: 'serverless-hello-world',
+        provider: {
+          name: 'tencent',
+          runtime: 'Node.js10.15',
+          memorySize: 128,
+          timeout: 3,
+        },
+        functions: {
+          index: {
+            handler: 'index.handler',
+            runtime: 'Node.js10.15',
+            timeout: 3,
+            memorySize: 128,
+            events: [
+              {
+                cos: {
+                  name: 'cos',
+                  parameters: {
+                    bucket: 'cli-appid.cos.ap-beijing.myqcloud.com',
+                    filter: { prefix: 'filterdir/', suffix: '.jpg' },
+                    events: 'cos:ObjectCreated:*',
+                    enable: false,
+                  },
+                },
+              },
+            ],
+          },
+        },
+      });
+    });
+
+    it('test transform cmq event', () => {
+      const result = generateFunctionsSpec(
+        path.join(__dirname, './fixtures/scf/f-event-cmq.yml')
+      );
+
+      assert.deepStrictEqual(result, {
+        service: 'serverless-hello-world',
+        provider: {
+          name: 'tencent',
+          runtime: 'Node.js10.15',
+          memorySize: 128,
+          timeout: 3,
+        },
+        functions: {
+          index: {
+            handler: 'index.handler',
+            runtime: 'Node.js10.15',
+            timeout: 3,
+            memorySize: 128,
+            events: [
+              {
+                cmq: {
+                  name: 'cmq',
+                  parameters: { enable: false, name: 'test-topic-queue' },
+                },
+              },
+            ],
+          },
+        },
+      });
+    });
   });
 });
