c3a0842f954ff10b612a39d3e5b99e1bf76d965c
echosoar <82163514@qq.com>
2020-04-30 09:49:51 +0800

    fix: remove urllib ws

---
+++ packages/faas-cli/bin/cli.js
@@ -0,0 +1,23 @@
+'use strict';
+const { debugWrapper } = require('@midwayjs/debugger');
+const cliFun = async argv => {
+  require('source-map-support/register');
+  const { CLI } = require('../dist');
+  const cli = new CLI(argv);
+  cli.start();
+};
+
+const cli = argv => {
+  const isDebug = argv.debug;
+  delete argv.debug;
+  debugWrapper({
+    file: __filename, // 要包裹的方法所在文件
+    export: 'cliFun', // 要包裹的方法的方法名
+    debug: isDebug,
+  })(argv);
+};
+
+module.exports = {
+  cliFun,
+  cli,
+};
